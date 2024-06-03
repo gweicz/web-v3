d0589c53bcb9617440f3bf473a0e8028875388cb
tree <tree@hidden.cz>
2024-06-03 23:28:07 +0200

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://v3.gwei.cz',
+  base: '/',
   integrations: [tailwind(), mdx()],
   vite: {
     plugins: [yaml()]

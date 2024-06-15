42ac6e29943cfc169645efedb6c4fffc04c4ab7a
tree <tree@hidden.cz>
2024-06-15 08:12:22 +0200

    fix mastodon link

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -20,8 +20,8 @@ export function renderRefs (refs, renderWeb = false) {
         out.push(`<a href=${'https://matrix.to/#/'+(refs.matrix[0] === "#" ? "" : "@") + refs.matrix} class="hover:underline">matrix</a>`)
     }
     if (refs.mastodon) {
-        const [ server, handle ] = refs.mastodon.split('@')
-        out.push(`<a href="${`https://elk.zone`}/${server}/${handle}" class="hover:underline">mastodon</a>`)
+        const [ handle, server ] = refs.mastodon.split('@')
+        out.push(`<a href="${`https://elk.zone`}/${server}/@${handle}" class="hover:underline">mastodon</a>`)
     }
     if (refs.signalMe) {
         out.push(`<a href=${refs.signalMe} class="hover:underline">signal</a>`)

02e62592446ea0731f75727a949d729303963c24
tree <tree@hidden.cz>
2024-07-18 20:48:57 +0200

    link

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -40,7 +40,8 @@ export function fixLink (url) {
     return `https://${url}`
 }
 
-export function shuffleArr (array){
+export function shuffleArr (oldArray){
+    const array = [...oldArray]
     for (var i = array.length - 1; i > 0; i--) {
         var rand = Math.floor(Math.random() * (i + 1));
         [array[i], array[rand]] = [array[rand], array[i]]
